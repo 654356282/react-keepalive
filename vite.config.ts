79645fee71ebb482e8dd81049d563e1bfa53976a
翁鸿添 <10440625+weng-hongtian@user.noreply.gitee.com>
2022-11-24 10:04:24 +0800

    chore: 构建更改

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import typescript from "@rollup/plugin-typescript";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig(({ command, mode }) => {
+  const plugins = [
     react(),
     typescript({
       target: "es5",
@@ -15,23 +16,33 @@ export default defineConfig({
       exclude: resolve("node_modules/**"),
       allowSyntheticDefaultImports: true,
     }),
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "ReactKeepalive",
-      fileName: "react-keepalive",
-      formats: ["cjs", "es", "umd"],
-    },
-    outDir: resolve(__dirname, "lib"),
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+  ];
+  if (mode === "analyzer") {
+    plugins.push(
+      visualizer({
+        open: true,
+      })
+    );
+  }
+  return {
+    plugins,
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "ReactKeepalive",
+        fileName: "react-keepalive",
+        formats: ["cjs", "es", "umd"],
+      },
+      outDir: resolve(__dirname, "lib"),
+      rollupOptions: {
+        external: ["react", "react-dom", "react/jsx-runtime"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
+  };
 });
