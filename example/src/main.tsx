6264ebc81d43991897d71414567530bd099d1531
wht <654356282@qq.com>
2022-12-04 12:46:57 +0800

    feat: 嵌套式路由示例

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./CompKeepalive";
 import RouterKeepalive from "./RouterKeepalive";
+import RouterKeepalive2 from './RouterKeepalive2'
 
 // const container = document.getElementById("root") as HTMLElement;
 // render(<App />, container);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RouterKeepalive />
+  <RouterKeepalive2 />
 );
